dc426be119a9f5124a9621b14f1f05cdf7d17946
Lyon <lozybean@163.com>
2015-09-14 00:28:27 +0800

    edit 01_get_singleton_read

--- src/MySeqRecord.py
+++ src/MySeqRecord.py
@@ -24,7 +24,7 @@ def __high_quality_count(self):
             q30_count += 1
     return q20_count,q30_count
 
-def find_sub_seq(self,seqrecord):
+def __find_longer(self,seqrecord):
     if ('count' not in self.__dict__) or ('count' not in seqrecord.__dict__):
         raise IOError,"to use this function, object must have attribute 'count'"
     seqa = seqrecord.seq
@@ -40,5 +40,5 @@ def find_sub_seq(self,seqrecord):
 SeqRecord.Q_ave = __Q_ave
 SeqRecord.N_count = __N_count
 SeqRecord.Q20_Q30 = __high_quality_count
-
+SeqRecord.find_longer = __find_longer
 
