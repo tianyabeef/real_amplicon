11e198688b50117c8b3b224496cb99c0e14c8805
Lyon <lozybean@163.com>
2015-11-12 14:43:33 +0800

    only link when use total sample

--- bin/downsize.py
+++ bin/downsize.py
@@ -1,5 +1,20 @@
 from settings import *
 
+def check_group_row(file1,file2):
+    count1 = 0
+    count2 = 0
+    with open(file1) as fp,open(file2) as fp2:
+        for line in fp:
+            if line.strip():
+                count1 += 1
+        for line in fp2:
+            if line.strip():
+                count2 += 1
+    if count1 == count2:
+        return True
+    else:
+        return False
+
 def downsize(cfg_in,vars=None):
     work = Work(DEFAULT_CONFIG_DIR + '/downsize.cfg')
     work.set_params(cfg_in,vars)
@@ -10,24 +25,37 @@ def downsize(cfg_in,vars=None):
     scripts = config.get_section('scripts')
     outfiles = config.get_section('outfiles')
 
-    params['group'] = params['group'] or None
-    if re.search('[t,y]',params['keep_small_size'].lower()):
-        keep_small_size = '--keep_small_size'
-    else:
-        keep_small_size = '--no_keep_small_size'
-    command = ('%s -s %s -i %s -o %s --out_statfile %s %s '%(scripts['downsize'],
+    flag = True
+    if params['pre_config']:
+        pre_config = MyConfigParser()
+        pre_config.readfp(open(params['pre_config']))
+        pre_outfiles = pre_config.get_section('outfiles')
+        pre_group = pre_config.get('params','group')
+        if check_group_row(pre_group,params['group']):
+            work.commands.append('ln -s %s %s'%(pre_outfiles['otu_table'],outfiles['otu_table']))
+            work.commands.append('ln -s %s %s'%(pre_outfiles['seqs_fa'],outfiles['seqs_fa']))
+            work.commands.append('ln -s %s %s'%(pre_outfiles['downsize_stat'],outfiles['downsize_stat']))
+            flag = False
+    if flag:
+        params['group'] = params['group'] or None
+
+        if re.search('[t,y]',params['keep_small_size'].lower()):
+            keep_small_size = '--keep_small_size'
+        else:
+            keep_small_size = '--no_keep_small_size'
+        command = ('%s -s %s -i %s -o %s --out_statfile %s %s '%(scripts['downsize'],
                                                                 params['stat_file_in'],
                                                                 params['otu_table_in'],
                                                                 outfiles['otu_table'],
                                                                 outfiles['downsize_stat'],
                                                                 keep_small_size))
-    if params['group']:
-        command += '-g %s'%params['group']
-    work.commands.append(command)
+        if params['group']:
+            command += '-g %s'%params['group']
+        work.commands.append(command)
 
-    work.commands.append('%s -i %s -t %s -o %s'%(scripts['otutab2fa'],
-                                                 params['seqs_all'],
-                                                 outfiles['otu_table'],
-                                                 outfiles['seqs_fa']))
+        work.commands.append('%s -i %s -t %s -o %s'%(scripts['otutab2fa'],
+                                                    params['seqs_all'],
+                                                    outfiles['otu_table'],
+                                                    outfiles['seqs_fa']))
 
     return outfiles
