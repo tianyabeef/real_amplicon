528974738e805a5a6ea7d3ca946e3ec4b81e68fd
Lyon <lozybean@163.com>
2015-12-25 16:54:21 +0800

    add paired diff

--- bin/taxanomy_diff.py
+++ bin/taxanomy_diff.py
@@ -3,47 +3,53 @@
 
 from settings import *
 
-def commands_factory(infile,outdir,mode,vars):
+
+def commands_factory(infile, outdir, mode, vars):
     scripts = vars['scripts']
     params = vars['params']
     commands = []
-    commands.append('%s -i %s -o %s -m %s'%(scripts['trans_profile'],
-                                             infile,
-                                             outdir + '/profile.for_analysis.txt',
-                                             mode))
-    commands.append('%s -i %s -o %s -g %s -c %s'%(scripts['tax_diff'],
-                                                  outdir + '/profile.for_analysis.txt',
-                                                  outdir,
-                                                  params['group'],
-                                                  params['p_cutoff']))
+    commands.append('%s -i %s -o %s -m %s' % (scripts['trans_profile'],
+                                              infile,
+                                              outdir + '/profile.for_analysis.txt',
+                                              mode))
+    command = '%s -i %s -o %s -g %s -c %s' % (scripts['tax_diff'],
+                                              outdir + '/profile.for_analysis.txt',
+                                              outdir,
+                                              params['group'],
+                                              params['p_cutoff'])
+    if params['paired'] != 'False':
+        command += ' --paired'
+    commands.append(command)
+
     # pca
     marker_profile = outdir + '/profile.for_plot.txt'
-    commands.append('%s -i %s -o %s -g %s'%(scripts['diff_pca'],
-                                            marker_profile,
-                                            outdir,
-                                            params['group']))
+    commands.append('%s -i %s -o %s -g %s' % (scripts['diff_pca'],
+                                              marker_profile,
+                                              outdir,
+                                              params['group']))
     # heatmap
-    commands.append('%s -f %s -o %s -g %s -t %s'%(scripts['diff_heatmap'],
-                                                marker_profile,
-                                                outdir,
-                                                params['group'],
-                                                params['heatmap_top']))
-    # boxplot
-    if vars['min_sample_num_in_groups'] >= 5:
-        commands.append('%s -i %s -o %s -g %s -t %s'%(scripts['diff_boxplot'],
+    commands.append('%s -f %s -o %s -g %s -t %s' % (scripts['diff_heatmap'],
                                                     marker_profile,
                                                     outdir,
                                                     params['group'],
-                                                    params['boxplot_top']))
-    os.system('mkdir -p %s'%outdir)
-    with open(outdir + '/commands.sh','w') as cmd:
+                                                    params['heatmap_top']))
+    # boxplot
+    if vars['min_sample_num_in_groups'] >= 5:
+        commands.append('%s -i %s -o %s -g %s -t %s' % (scripts['diff_boxplot'],
+                                                        marker_profile,
+                                                        outdir,
+                                                        params['group'],
+                                                        params['boxplot_top']))
+    os.system('mkdir -p %s' % outdir)
+    with open(outdir + '/commands.sh', 'w') as cmd:
         cmd.write('\n'.join(commands))
 
     return commands
 
-def taxanomy_diff(cfg_in,vars=None):
+
+def taxanomy_diff(cfg_in, vars=None):
     work = Work(DEFAULT_CONFIG_DIR + '/taxanomy_diff.cfg')
-    work.set_params(cfg_in,vars)
+    work.set_params(cfg_in, vars)
     work.load_default_config()
 
     config = work.config
@@ -51,45 +57,46 @@ def taxanomy_diff(cfg_in,vars=None):
     scripts = config.get_section('scripts')
     outfiles = config.get_section('outfiles')
     softwares = config.get_section('softwares')
-    sample_num_in_groups,\
-    min_sample_num_in_groups,\
-    sample_num_total,\
+    sample_num_in_groups, \
+    min_sample_num_in_groups, \
+    sample_num_total, \
     group_num = parse_group(params['group'])
 
     # min_sample_num_in_groups must be more than 3
     if min_sample_num_in_groups < 3:
         return outfiles
 
-    #  LEfSe analysis
-    work.commands.append('%s -i %s -l %s -g %s -o %s --LDA %s'%(scripts['LEfSe'],
-                                                                params['summarize_dir'] + '/otu_table_all.txt',
-                                                                softwares['LEfSe_dir'],
-                                                                params['group'],
-                                                                outfiles['LEfSe_outdir'],
-                                                                params['LDA_cutoff']))
+    # LEfSe analysis
+    work.commands.append('%s -i %s -l %s -g %s -o %s --LDA %s' % (scripts['LEfSe'],
+                                                                  params['summarize_dir'] + '/otu_table_all.txt',
+                                                                  softwares['LEfSe_dir'],
+                                                                  params['group'],
+                                                                  outfiles['LEfSe_outdir'],
+                                                                  params['LDA_cutoff']))
     vars = {
-        'min_sample_num_in_groups' : min_sample_num_in_groups,
-        'sample_num_total' : sample_num_total,
-        'scripts' : scripts,
-        'params' : params,
+        'min_sample_num_in_groups': min_sample_num_in_groups,
+        'sample_num_total': sample_num_total,
+        'scripts': scripts,
+        'params': params,
     }
     # otu level
-    work.commands += commands_factory(params['uniform_profile'],outfiles['otu_diff_outdir'],'otu',vars)
+    work.commands += commands_factory(params['uniform_profile'], outfiles['otu_diff_outdir'], 'otu', vars)
 
     # all level
-    work.commands += commands_factory(params['summarize_dir'] + '/otu_table_all.txt',outfiles['taxall_diff_outdir'],'all',vars)
+    work.commands += commands_factory(params['summarize_dir'] + '/otu_table_all.txt', outfiles['taxall_diff_outdir'],
+                                      'all', vars)
 
     # phylum level
-    work.commands += commands_factory(params['summarize_dir'] + '/otu_table_L2.txt',outfiles['phylum_diff_outdir'],'phylum',vars)
+    work.commands += commands_factory(params['summarize_dir'] + '/otu_table_L2.txt', outfiles['phylum_diff_outdir'],
+                                      'phylum', vars)
 
     # genus level
-    work.commands += commands_factory(params['summarize_dir'] + '/otu_table_L6.txt',outfiles['genus_diff_outdir'],'genus',vars)
-
+    work.commands += commands_factory(params['summarize_dir'] + '/otu_table_L6.txt', outfiles['genus_diff_outdir'],
+                                      'genus', vars)
 
     return outfiles
+
 
 if __name__ == '__main__':
     config = sys.argv[1]
     taxanomy_diff(config)
-
-
--- script/05_tax_diff.py
+++ script/05_tax_diff.py
@@ -5,20 +5,25 @@ import sys
 import argparse
 import os
 from util import mkdir
+
 this_script_path = os.path.dirname(__file__)
-sys.path.insert(1,this_script_path + '/../src')
+sys.path.insert(1, this_script_path + '/../src')
 import Parser as rp
+
 
 def read_params(args):
     parser = argparse.ArgumentParser(description='''tax diff | v1.0 at 2015/10/26 by liangzb ''')
-    parser.add_argument('-i','--infile',dest='infile',metavar='FILE',type=str,required=True,
+    parser.add_argument('-i', '--infile', dest='infile', metavar='FILE', type=str, required=True,
                         help="set the transed file")
-    parser.add_argument('-o','--outdir',dest='outdir',metavar='DIR',type=str,required=True,
+    parser.add_argument('-o', '--outdir', dest='outdir', metavar='DIR', type=str, required=True,
                         help="set the work dir")
-    parser.add_argument('-g','--group',dest='group',metavar='FILE',type=str,required=True,
+    parser.add_argument('-g', '--group', dest='group', metavar='FILE', type=str, required=True,
                         help="set the group file")
-    parser.add_argument('-c','--cutoff',dest='cutoff',metavar='FILE',type=float,default=0.05,
+    parser.add_argument('-c', '--cutoff', dest='cutoff', metavar='FILE', type=float, default=0.05,
                         help="set the p_value cutoff")
+    parser.add_argument('--paired', dest='paired', action='store_true',
+                        help="paired compare")
+    parser.set_defaults(with_boxplot=False)
     args = parser.parse_args()
     params = vars(args)
     params['marker'] = params['outdir'] + '/diff.marker.tsv'
@@ -26,15 +31,16 @@ def read_params(args):
     params['profile'] = params['outdir'] + '/profile.for_plot.txt'
     return params
 
-def filt(infile,filt_file):
+
+def filt(infile, filt_file):
     marker_set = set()
-    with open(infile) as fp,open(filt_file,'w') as out:
+    with open(infile) as fp, open(filt_file, 'w') as out:
         out.write(fp.next())
         for line in fp:
             tabs = line.strip().split('\t')
             try:
                 p_value = float(tabs[-1])
-            except ValueError,ex:
+            except ValueError, ex:
                 if str(ex).startswith('could not convert string to float'):
                     continue
             if p_value < params['cutoff']:
@@ -42,8 +48,9 @@ def filt(infile,filt_file):
                 out.write(line)
     return marker_set
 
-def get_profile(marker_set,infile,outfile):
-    with open(infile) as fp, open(outfile,'w') as out:
+
+def get_profile(marker_set, infile, outfile):
+    with open(infile) as fp, open(outfile, 'w') as out:
         out.write(fp.next())
         for line in fp:
             name = line.split('\t')[0]
@@ -57,12 +64,13 @@ if __name__ == '__main__':
     r_job = rp.Rparser()
     r_job.open(this_script_path + '/../src/template/05_diff_test.Rtp')
     var = {
-        'infile' : params['infile'],
-        'group_file' : params['group'],
-        'marker_file' : params['marker']
+        'infile': params['infile'],
+        'group_file': params['group'],
+        'marker_file': params['marker'],
+        'pair': params['paired'],
     }
     r_job.format(var)
     r_job.write(params['outdir'] + '/diff.marker.R')
     r_job.run()
-    marker_set = filt(params['marker'],params['filt'])
-    get_profile(marker_set,params['infile'],params['profile'])
+    marker_set = filt(params['marker'], params['filt'])
+    get_profile(marker_set, params['infile'], params['profile'])
