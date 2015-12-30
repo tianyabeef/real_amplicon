044b34c08cbcee5e45e03b8c4d6d31d927c37a70
Lyon <lozybean@163.com>
2015-12-30 17:26:41 +0800

    fix alpha group bug

--- script/04_alpha_group.py
+++ script/04_alpha_group.py
@@ -6,17 +6,19 @@ import sys
 import argparse
 import os
 from util import mkdir
+
 this_script_path = os.path.dirname(__file__)
-sys.path.insert(1,this_script_path + '/../src')
+sys.path.insert(1, this_script_path + '/../src')
 from Parser import parse_group_file
+
 
 def read_params(args):
     parser = argparse.ArgumentParser(description='''alpha grouped | v1.0 at 2015/10/15 by liangzb ''')
-    parser.add_argument('-a','--alpha_collate_dir',dest='alpha_dir',metavar='DIR', type=str, required=True,
+    parser.add_argument('-a', '--alpha_collate_dir', dest='alpha_dir', metavar='DIR', type=str, required=True,
                         help="set the alpha_collate_dir")
-    parser.add_argument('-g','--group',dest='group',metavar='FILE',type=str,required=True,
+    parser.add_argument('-g', '--group', dest='group', metavar='FILE', type=str, required=True,
                         help="set the group file")
-    parser.add_argument('-m','--alpha_metrics',dest='alpha_metrics',metavar='STR',type=str,
+    parser.add_argument('-m', '--alpha_metrics', dest='alpha_metrics', metavar='STR', type=str,
                         default="chao1,observed_species,PD_whole_tree,shannon,simpson,goods_coverage",
                         help="set the alpha_metrics split by ',',\
                               [default is chao1,observed_species,PD_whole_tree,shannon,simpson,goods_coverage] ")
@@ -26,35 +28,38 @@ def read_params(args):
     params = vars(args)
     return params
 
-def work_single(file,group):
+
+def work_single(file, group):
     alphas = {}
     with open(file) as fp:
-        samples = fp.next().strip().split('\t')[3:]
+        samples = fp.next().rstrip().split('\t')[3:]
         for tail in fp:
             pass
-        tail = tail.strip().split()[3:]
-    for ind,sample in enumerate(samples):
+        tail = tail.rstrip().split()[3:]
+    for ind, sample in enumerate(samples):
         group_name = group[sample]
         if group_name not in alphas:
             alphas[group_name] = []
         alphas[group_name].append(tail[ind])
     return alphas
 
-def write(outfile,alphas,group):
+
+def write(outfile, alphas, group):
     group_sort = list(group.itervalues())
-    group_names = sorted(set(group_sort),key=group_sort.index)
-    with open(outfile,'w') as out:
+    group_names = sorted(set(group_sort), key=group_sort.index)
+    with open(outfile, 'w') as out:
         for group_name in group_names:
             alpha_list = alphas[group_name]
             alpha_list = '\t'.join(alpha_list)
-            out.write('%s\t%s\n'%(group_name,alpha_list))
+            out.write('%s\t%s\n' % (group_name, alpha_list))
+
 
 if __name__ == '__main__':
     params = read_params(sys.argv)
     mkdir(params['out_dir'])
     group = parse_group_file(params['group'])
     for alpha_name in params['alpha_metrics'].split(','):
-        file = '%s/%s.txt'%(params['alpha_dir'],alpha_name)
-        outfile = '%s/%s.txt'%(params['out_dir'],alpha_name)
-        alphas = work_single(file,group)
-        write(outfile,alphas,group)
+        file = '%s/%s.txt' % (params['alpha_dir'], alpha_name)
+        outfile = '%s/%s.txt' % (params['out_dir'], alpha_name)
+        alphas = work_single(file, group)
+        write(outfile, alphas, group)
--- src/SumProfileTree.py
+++ src/SumProfileTree.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*- #
 
 import sys
-from Parser import TableParser,TaxParser,parse_group_file
+from Parser import TableParser, TaxParser, parse_group_file
+
 
 class Node(object):
-    def __init__(self,level,name,samples,groups=None):
+    def __init__(self, level, name, samples, groups=None):
         self.sub_node = {}
         self.name = name
         self.level = level
@@ -19,8 +20,7 @@ class Node(object):
 
 
 class Subject(object):
-
-    def __init__(self,profile,taxfile,outfile,group_file=None,expand=1):
+    def __init__(self, profile, taxfile, outfile, group_file=None, expand=1):
         self.table = TableParser(profile)
         self.otu_tax = TaxParser(taxfile)
         self.group = parse_group_file(group_file)
@@ -30,15 +30,15 @@ class Subject(object):
             self.groups = None
         self.outfile = outfile
         self.expand = expand
-        self.tax_level = ['kingdom','phylum','class','order','family','genus','species']
+        self.tax_level = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']
         self.tax_dict = {
-            'k':'kingdom',
-            'p':'phylum',
-            'c':'class',
-            'o':'order',
-            'f':'family',
-            'g':'genus',
-            's':'species',
+            'k': 'kingdom',
+            'p': 'phylum',
+            'c': 'class',
+            'o': 'order',
+            'f': 'family',
+            'g': 'genus',
+            's': 'species',
         }
         self.samples = {}
         self.node_list = {}
@@ -55,7 +55,7 @@ class Subject(object):
         self.otu_tax = self.otu_tax.OTUs
 
     def get_tax_tree(self):
-        for otu,otu_tax in self.otu_tax.iteritems():
+        for otu, otu_tax in self.otu_tax.iteritems():
             root_level = self.tax_level[0]
             father_level = root_level
             try:
@@ -67,11 +67,11 @@ class Subject(object):
                 else:
                     continue
             if tax_name not in self.root_nodes:
-                root_node = Node(root_level,tax_name,self.samples,groups=self.groups)
+                root_node = Node(root_level, tax_name, self.samples, groups=self.groups)
                 self.node_list[tax_name] = root_node
                 self.root_nodes[tax_name] = root_node
             levels = list(otu_tax.iterkeys())
-            for ind,level in enumerate(levels):
+            for ind, level in enumerate(levels):
                 if level not in otu_tax:
                     continue
                 father_node = self.node_list[otu_tax[father_level]]
@@ -79,17 +79,17 @@ class Subject(object):
                 tax_name = otu_tax[level]
                 if tax_name in self.node_list:
                     continue
-                node = Node(level,tax_name,self.samples,groups=self.groups)
+                node = Node(level, tax_name, self.samples, groups=self.groups)
                 self.node_list[tax_name] = node
                 if tax_name not in father_node.sub_node:
                     father_node.sub_node[tax_name] = node
 
-    '''TODO:
-        simplify beblow functions
-    '''
-    def get_node_profile(self,node):
+    # TODO:
+    #     simplify beblow functions
+    #
+    def get_node_profile(self, node):
         for sample in self.samples:
-            for otu,otu_tax in self.otu_tax.iteritems():
+            for otu, otu_tax in self.otu_tax.iteritems():
                 if node.level not in otu_tax or otu_tax[node.level] != node.name:
                     continue
                 try:
@@ -105,15 +105,15 @@ class Subject(object):
         for node in self.root_nodes.itervalues():
             self.get_node_profile(node)
 
-    def get_node_group_profile(self,node):
+    def get_node_group_profile(self, node):
         deleted = set()
-        for sample,group in self.group.iteritems():
-            for otu,otu_tax in self.otu_tax.iteritems():
+        for sample, group in self.group.iteritems():
+            for otu, otu_tax in self.otu_tax.iteritems():
                 if node.level not in otu_tax or otu_tax[node.level] != node.name:
                     continue
                 try:
                     node.group_profile[group] += self.table.samples[sample][otu]
-                except KeyError,ex:
+                except KeyError, ex:
                     sample_name = str(ex).strip("'")
                     if sample_name not in deleted:
                         sys.stderr.write(sample_name)
@@ -130,7 +130,7 @@ class Subject(object):
         for node in self.root_nodes.itervalues():
             self.get_node_group_profile(node)
 
-    def get_node_uniform(self,node):
+    def get_node_uniform(self, node):
         if not node.sub_node:
             return None
         for sample in self.samples:
@@ -145,7 +145,7 @@ class Subject(object):
         for sub_node in node.sub_node.itervalues():
             self.get_node_uniform(sub_node)
 
-    def get_uniform(self,expand=1):
+    def get_uniform(self, expand=1):
         for sample in self.samples:
             total = 0
             for node in self.root_nodes.itervalues():
@@ -156,7 +156,7 @@ class Subject(object):
         for node in self.root_nodes.itervalues():
             self.get_node_uniform(node)
 
-    def get_node_group_uniform(self,node):
+    def get_node_group_uniform(self, node):
         if not node.sub_node:
             return None
         for group in self.groups:
@@ -171,7 +171,7 @@ class Subject(object):
         for sub_node in node.sub_node.itervalues():
             self.get_node_group_uniform(sub_node)
 
-    def get_group_uniform(self,expand=1):
+    def get_group_uniform(self, expand=1):
         for group in self.groups:
             total = 0
             for node in self.root_nodes.itervalues():
@@ -182,42 +182,41 @@ class Subject(object):
         for node in self.root_nodes.itervalues():
             self.get_node_group_uniform(node)
 
-
-    def write_node(self,node,fp):
+    def write_node(self, node, fp):
         level_name = self.tax_dict[node.name[0]]
         level_ind = self.tax_level.index(level_name)
-        out_str = '\t'*level_ind + node.name[3:] + '\t'*(6-level_ind)
+        out_str = '\t' * level_ind + node.name[3:] + '\t' * (6 - level_ind)
         for sample in self.samples:
-            out_str += '\t%s'%node.profile[sample]
+            out_str += '\t%s' % node.profile[sample]
         fp.write(out_str + '\n')
         if not node.sub_node:
             return None
         for sub_node in node.sub_node.itervalues():
-            self.write_node(sub_node,fp)
+            self.write_node(sub_node, fp)
 
     def write_tree(self):
-        with open(self.outfile,'w') as fp:
+        with open(self.outfile, 'w') as fp:
             fp.write('\t'.join(self.tax_level) + '\t' + '\t'.join(self.samples) + '\n')
             for node in self.root_nodes.itervalues():
-                self.write_node(node,fp)
+                self.write_node(node, fp)
 
-    def write_group_node(self,node,fp):
+    def write_group_node(self, node, fp):
         level_name = self.tax_dict[node.name[0]]
         level_ind = self.tax_level.index(level_name)
-        out_str = '\t'*level_ind + node.name[3:] + '\t'*(6-level_ind)
+        out_str = '\t' * level_ind + node.name[3:] + '\t' * (6 - level_ind)
         for group in self.groups:
-            out_str += '\t%s'%node.group_profile[group]
+            out_str += '\t%s' % node.group_profile[group]
         fp.write(out_str + '\n')
         if not node.sub_node:
             return None
         for sub_node in node.sub_node.itervalues():
-            self.write_group_node(sub_node,fp)
+            self.write_group_node(sub_node, fp)
 
     def write_group_tree(self):
-        with open(self.outfile,'w') as fp:
+        with open(self.outfile, 'w') as fp:
             fp.write('\t'.join(self.tax_level) + '\t' + '\t'.join(self.groups) + '\n')
             for node in self.root_nodes.itervalues():
-                self.write_group_node(node,fp)
+                self.write_group_node(node, fp)
 
     def work(self):
         self.read_table()
