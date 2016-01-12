82e3302045d5da44a545645ee7c04d5e63fe70b6
Lyon <lozybean@163.com>
2016-01-12 11:53:10 +0800

    change reault directory maker

--- bin/get_result.py
+++ bin/get_result.py
@@ -11,13 +11,17 @@ import sys
 import re
 
 
+def makedir(dir):
+    if os.path.isdir(dir):
+        return None
+    if dir == '':
+        return None
+    os.system('mkdir -p %s' % dir)
+
+
 def makedirs(dirs):
     for dir in dirs:
-        if os.path.isdir(dir):
-            return None
-        if dir == '':
-            return None
-        os.system('mkdir -p %s' % dir)
+        makedir(dir)
 
 
 def get_result(cfg_in, vars=None):
@@ -32,42 +36,13 @@ def get_result(cfg_in, vars=None):
     out_dir_results = outfiles['out_dir']
 
     os.system('rm -Rf %s' % out_dir_results)
-    dirs = []
-    dirs.append(out_dir_results + "/01_Reads/")
-    dirs.append(out_dir_results + "/02_OTU/all/wf_taxa_summary/")
-    dirs.append(out_dir_results + "/02_OTU/all/bar_plot/phylum/")
-    dirs.append(out_dir_results + "/02_OTU/all/bar_plot/order/")
-    dirs.append(out_dir_results + "/02_OTU/all/bar_plot/class/")
-    dirs.append(out_dir_results + "/02_OTU/all/bar_plot/genus/")
-    dirs.append(out_dir_results + "/02_OTU/all/bar_plot/family/")
-    dirs.append(out_dir_results + "/03_Alpha_diversity/total_alpha_rare/")
-    dirs.append(out_dir_results + "/04_Beta_diversity/")
-    dirs.append(out_dir_results + "/05_diff_analysis/")
-    makedirs(dirs)
+
     origin = config.items('origin')
     command = ''
     group_files = re.split('\s+', config.get('params', 'group_files'))
 
     regex = re.compile('.+\/(.+)\..+')
 
-    for group_file in group_files:
-        analysis_name = re.search(regex, group_file).group(1)
-        dirs = []
-        dirs.append(out_dir_results + "/02_OTU/" + analysis_name + "/wf_taxa_summary/")
-        dirs.append(out_dir_results + "/02_OTU/" + analysis_name + "/bar_plot/phylum/")
-        dirs.append(out_dir_results + "/02_OTU/" + analysis_name + "/bar_plot/order/")
-        dirs.append(out_dir_results + "/02_OTU/" + analysis_name + "/bar_plot/class/")
-        dirs.append(out_dir_results + "/02_OTU/" + analysis_name + "/bar_plot/genus/")
-        dirs.append(out_dir_results + "/02_OTU/" + analysis_name + "/bar_plot/family/")
-        dirs.append(out_dir_results + "/03_Alpha_diversity/" + analysis_name)
-        dirs.append(out_dir_results + "/04_Beta_diversity/" + analysis_name)
-        dirs.append(out_dir_results + "/03_Alpha_diversity/" + analysis_name + "/box_plot/")
-        dirs.append(out_dir_results + "/05_diff_analysis/" + analysis_name + "/LEfSe/")
-        dirs.append(out_dir_results + "/05_diff_analysis/" + analysis_name + "/otu_diff/")
-        dirs.append(out_dir_results + "/05_diff_analysis/" + analysis_name + "/genus_diff/")
-        dirs.append(out_dir_results + "/05_diff_analysis/" + analysis_name + "/taxall_diff/")
-        dirs.append(out_dir_results + "/05_diff_analysis/" + analysis_name + "/phylum_diff/")
-        makedirs(dirs)
     for key, value in origin:
         if config.has_option('target', key):
             match = re.match(r".*#group.*", value)
@@ -75,19 +50,23 @@ def get_result(cfg_in, vars=None):
                 for group_file in group_files:
                     analysis_name = re.search(regex, group_file).group(1)
                     value_rep = value.replace("#group", analysis_name)
-                    command += 'cp -rf ' + work_dir + '../' + value_rep + ' ' + out_dir_results
-                    command += config.get('target', key).replace("#group", analysis_name) + '\n'
+                    target_file = out_dir_results + config.get('target', key).replace("#group", analysis_name)
+                    makedir(os.path.dirname(target_file))
+                    command += 'cp -rf ' + work_dir + '../' + value_rep + ' '
+                    command += target_file + '\n'
             else:
-                command += 'cp -rf ' + work_dir + '../' + value + ' ' + out_dir_results + config.get('target',
-                                                                                                     key) + '\n'
+                target_file = out_dir_results + config.get('target', key)
+                makedir(os.path.dirname(target_file))
+                command += 'cp -rf ' + work_dir + '../' + value + ' ' + target_file + '\n'
         else:
             sys.stderr.write('06_get_html.cfg target section no have ' + key + '\n')
     work.commands.append(command)
     for docs in os.listdir(params['docs_dir']):
         if docs.find(params['data_type']) >= 0:
             origin = '%s/%s' % (params['docs_dir'], docs)
-            target = '%s/%s' % (outfiles['out_dir'], docs)
-            work.commands.append('cp -rf %s %s' % (origin, target))
+            target_file = '%s/%s' % (outfiles['out_dir'], docs)
+            makedir(os.path.dirname(target_file))
+            work.commands.append('cp -rf %s %s' % (origin, target_file))
     return outfiles
 
 
