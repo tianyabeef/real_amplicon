2c76cd67efad626d348f61452fe7bbe8253f7865
Lyon <lozybean@163.com>
2016-01-15 16:59:58 +0800

    change venn to adjust genus

--- script/03_otu_venn.py
+++ script/03_otu_venn.py
@@ -4,7 +4,8 @@ import sys
 import argparse
 import os
 import re
-from util import mkdir,image_trans
+from util import mkdir, image_trans
+
 this_script_path = os.path.dirname(__file__)
 sys.path.insert(1, this_script_path + '/../src')
 import Parser as rp
@@ -22,15 +23,18 @@ def read_params(args):
                         help="set the output dir")
     args = parser.parse_args()
     params = vars(args)
-    params['group_dir']=params['group']
+    params['group_dir'] = params['group']
     params['group'] = parse_group_file(params['group'])
     return params
 
-def read(otu_table_file,sample_in_group,vars):
+
+def read(otu_table_file, sample_in_group, vars):
     otu_in_group = OrderedDict()
     group_names = set(sample_in_group.itervalues())
-    for ind,group in enumerate(list(group_names)):
-        vars['group_name%s'%(ind+1,)] = group
+    otu_num_dict = {}
+    otu_count = 1
+    for ind, group in enumerate(list(group_names)):
+        vars['group_name%s' % (ind + 1,)] = group
         otu_in_group[group] = set()
     with open(otu_table_file) as otu_table:
         head = otu_table.next()
@@ -40,18 +44,26 @@ def read(otu_table_file,sample_in_group,vars):
         for line in otu_table:
             tabs = line.strip().split('\t')
             otu_name = tabs.pop(0)
-            otu_num = re.search('(\d+)$',otu_name).group(1)
-            for ind,tab in enumerate(tabs):
+            try:
+                otu_num = re.search('(\d+)$', otu_name).group(1)
+            except AttributeError:
+                if otu_name not in otu_num_dict:
+                    otu_num_dict[otu_name] = str(otu_count)
+                    otu_count += 1
+                otu_num = otu_num_dict[otu_name]
+            for ind, tab in enumerate(tabs):
                 if float(tab) > 0:
                     group = sample_in_group[samples[ind]]
                     otu_in_group[group].add(otu_num)
     return otu_in_group
 
-def write(otu_in_group,outfile):
-    with open(outfile,'w') as fp:
-        for group,otus in otu_in_group.iteritems():
-            otus = sorted(list(otus),cmp=lambda a,b:cmp(int(a),int(b)))
-            fp.write('%s\t%s\n'%(group,' '.join(otus)))
+
+def write(otu_in_group, outfile):
+    with open(outfile, 'w') as fp:
+        for group, otus in otu_in_group.iteritems():
+            otus = sorted(list(otus), cmp=lambda a, b: cmp(int(a), int(b)))
+            fp.write('%s\t%s\n' % (group, ' '.join(otus)))
+
 
 if __name__ == '__main__':
     params = read_params(sys.argv)
@@ -59,17 +71,16 @@ if __name__ == '__main__':
     for_plot = params['out_dir'] + '/for_plot.txt'
     tiff_file = params['out_dir'] + '/venn.tiff'
     png_file = params['out_dir'] + '/venn.png'
-    vars = {'for_plot':for_plot,
-            'tiff_file':tiff_file,
-            'group_file':params['group_dir']}
+    vars = {'for_plot': for_plot,
+            'tiff_file': tiff_file,
+            'group_file': params['group_dir']}
 
-    otu_in_group = read(params['otu_table'],params['group'],vars)
-    write(otu_in_group,for_plot)
+    otu_in_group = read(params['otu_table'], params['group'], vars)
+    write(otu_in_group, for_plot)
 
     r_job = rp.Rparser()
     r_job.open(this_script_path + '/../src/template/03_otu_venn.Rtp')
     r_job.format(vars)
     r_job.write(params['out_dir'] + '/otu_venn.R')
     r_job.run()
-    image_trans(tiff_file,png_file)
-
+    image_trans(tiff_file, png_file)
